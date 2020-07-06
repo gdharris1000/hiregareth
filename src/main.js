655d83251599f33be17fa154d5fa9327a23b3d51
Gareth Harris <gdharris1000@gmail.com>
2020-07-06 21:35:06 +0100

    added vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import HomeScreen from './components/HomeScreen'
 
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/', component: HomeScreen}
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
