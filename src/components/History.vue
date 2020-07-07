af759bab348a37c4b600c347d124a91f5c19c2b9
Gareth Harris <gdharris1000@gmail.com>
2020-07-07 17:33:07 +0100

    history text added

--- src/components/History.vue
+++ src/components/History.vue
@@ -45,6 +45,145 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">SDNI Cosmo<br>(Tokyo)</div>
+                            <div class="col-md-6 text-right">Feb 2017 – Apr 2017</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Web Designer (Part Time)</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Translated Japanese website into English</li>
+                                <li>Created a new English-language website.</li>
+                                <li>Managed communication with English-speaking clients</li>
+                                <li>Translated documents from Japanese to English</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">ARC Academy<br>(Tokyo)</div>
+                            <div class="col-md-6 text-right">Jul 2015 – Apr 2017</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Japanese Language School</h5>
+                       
+                        <p class="card-text">
+                            <ul>
+                                <li>Studied Japanese Language</li>
+                                <li>Achieved JLPT N3</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">Hays (contracted to NHS)<br>(Cambridge)</div>
+                            <div class="col-md-6 text-right">Feb 2015 – Jun 2015</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Web Officer</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Provided first line business support for queries around the NHS Health Education East of England internet site.</li>
+                                <li>Developed and delivered training programmes in the use of website and intranet tools.</li>
+                                <li>Maintained and reported site use.</li>
+                                <li>Developed a support logging process.</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">BP plc.<br>(Hemel Hempstead)</div>
+                            <div class="col-md-6 text-right">Jan 2014 – Sep 2014</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Business Analyst / Project Administrator</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Provided first line business support for queries around the Air BP internet site prior to its migration to a new platform.</li>
+                                <li>Worked in close partnership with the Project Manager to build and transfer content to the new Air BP internet site.</li>
+                                <li>Supported IT&amp;S and business stakeholders in the delivery of new Air BP online applications by:</li>
+                                <ul>
+                                    <li>Acting as subject matter expert in the project team</li>
+                                    <li>Gathering business requirements and turning them into system requirements</li>
+                                    <li>Testing of new releases</li>
+                                    <li>Documenting support processes</li>
+                                    <li>Training support teams</li>
+                                </ul>
+                                <li>Provided the link between the Air BP Communications team and the Internet Development team, ensuring that solutions were found to address Air BP’s functionality requirements.</li>
+                                <li>Ran training sessions for web page owners on how to use the Air BP internet site’s content management system (Adobe CQ5)</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">BP plc.<br>(Hemel Hempstead)</div>
+                            <div class="col-md-6 text-right">Jul 2007 – Jan 2014</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Internet and intranet services expert</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Provided first line business support for the Air BP global internet, intranet and SharePoint websites</li>
+                                <li>Managed updates, enhancements and site development</li>
+                                <li>User acceptance testing</li>
+                                <li>Reporting of site usage statistics</li>
+                                <li>Providing first line business support for a number of bespoke IT systems</li>
+                                <li>Incident management</li>
+                                <li>Provided training and consultation around systems that improved business operations and efficiency</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">Select Appointments (contracted to BP plc.)<br>(Sunbury-on-Thames)</div>
+                            <div class="col-md-6 text-right">Feb 2004 – Jul 2007</div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Web Administrator</h5>
+                        <p class="card-text">
+                            <ul>
+                                <li>Maintaining the Air BP internet, intranet and SharePoint sites</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -55,5 +194,7 @@ export default {
 </script>
 
 <style scoped>
-
+    .row {
+        margin-top: 20px;
+    }
 </style>
