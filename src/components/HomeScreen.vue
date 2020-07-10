40ce60d625df2692e7696093615ddeb7dcb24a30
Gareth Harris <gdharris1000@gmail.com>
2020-07-10 09:23:15 +0100

    hover over tech logos

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -5,38 +5,49 @@
                
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/flutter.png" alt="Flutter">
+                <img src="../../public/images/icons/flutter.png" alt="Flutter" class="img-front">
+                <img src="../../public/images/icons/flutter2.png" alt="Flutter" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/dart.png" alt="Dart">
+                <img src="../../public/images/icons/dart.png" alt="Dart" class="img-front">
+                <img src="../../public/images/icons/dart2.png" alt="Dart" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/ruby.png" alt="Ruby">
+                <img src="../../public/images/icons/ruby.png" alt="Ruby" class="img-front">
+                <img src="../../public/images/icons/ruby2.png" alt="Ruby" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/rails.png" alt="Rails">
+                <img src="../../public/images/icons/rails.png" alt="Rails" class="img-front">
+                <img src="../../public/images/icons/rails2.png" alt="Rails" class="img-back">
+                
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/psql.png" alt="PostgreSQL">
+                <img src="../../public/images/icons/psql.png" alt="PostgreSQL" class="img-front">
+                <img src="../../public/images/icons/psql2.png" alt="PostgreSQL" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/html.png" alt="HTML">
+                <img src="../../public/images/icons/html.png" alt="HTML" class="img-front">
+                <img src="../../public/images/icons/html2.png" alt="HTML" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/css.png" alt="CSS">
+                <img src="../../public/images/icons/css.png" alt="CSS" class="img-front">
+                <img src="../../public/images/icons/css2.png" alt="CSS" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/javascript.png" alt="JavaScript">
+                <img src="../../public/images/icons/javascript.png" alt="JavaScript" class="img-front">
+                <img src="../../public/images/icons/javascript2.png" alt="JavaScript" class="img-back">
             </div>
             <div class="col-md-1">
-                <img src="../../public/images/icons/firebase.png" alt="Firebase">
+                <img src="../../public/images/icons/firebase.png" alt="Firebase" class="img-front">
+                <img src="../../public/images/icons/firebase2.png" alt="Firebase" class="img-back">
             </div>
             <!-- <div class="col-md-1">
                 <img src="../../public/images/icons/bootstrap.png" alt="Bootstrap"><br>
                 Bootstrap
             </div> -->
             <div class="col-md-1">
-                <img src="../../public/images/icons/vue.png" alt="Vue">
+                <img src="../../public/images/icons/vue.png" alt="Vue" class="img-front">
+                <img src="../../public/images/icons/vue2.png" alt="Vue" class="img-back">
             </div>
             <div class="col-md-1">
                
@@ -60,4 +71,18 @@ img{
     font-weight: bold;
     padding: 5px;
 }
+
+.img-back{
+    display: none;
+}
+
+.col-md-1:hover .img-back{
+    display: inline !important;
+}
+
+.col-md-1:hover .img-front{
+    display: none !important;
+}
+
+
 </style>
